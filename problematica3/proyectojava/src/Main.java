0291f5410769110c7670dc7ccd76a47f9be49bb8
Freddy Chamorro <fachamorroe@unl.edu.ec>
2022-10-15 13:57:51 -0500

    programa codigo

--- problematica3/proyectojava/src/Main.java
+++ problematica3/proyectojava/src/Main.java
@@ -8,11 +8,13 @@ public class Main {
         System.out.println("Bienvenido");
         Municipio municipio = new Municipio("Municipio de Loja");
         //Cliente cliente = new Cliente(null, null, 0, null, municipio);
-        Cliente cliente = new Cliente("Carlos", "Perez", 1105050304, "Loja", municipio);
-        Medidor medidor = new Medidor("1", 1, "Loja", cliente);
-        cliente.comprarMedidor(medidor);
-        Medidor medidor2 = new Medidor("2", 2, "Loja", cliente);
-        cliente.comprarMedidor(medidor2);
+        Cliente cliente1 = new Cliente("Carlos", "Perez", 1105050304, "Loja", municipio);
+        Medidor medidor = new Medidor("1", 1, "Loja", cliente1);
+        //cliente1.comprarMedidor(medidor);
+        Medidor medidor2 = new Medidor("2", 2, "Malacatos", cliente1);
+        //cliente1.comprarMedidor(medidor);
+        System.out.println(cliente1.toString());
+        System.out.println(cliente1.getMedidors());
         
 
     }
